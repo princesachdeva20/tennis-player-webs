666258810c280763c3ce0e3f22424533d22b0f4b
princesachdeva20 <52614846+princesachdeva20@users.noreply.github.com>
2023-05-31 20:39:56 -0400

    new info jsp file

--- src/main/java/PlayerServlet.java
+++ src/main/java/PlayerServlet.java
@@ -7,14 +7,24 @@ import java.io.IOException;
 import java.io.PrintWriter;
 @WebServlet(urlPatterns = "/player.do")
 public class PlayerServlet extends HttpServlet {
-    @Override
+    /*@Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         String playerfirstName = request.getParameter("firstname");
         System.out.println(playerfirstName);
         String playerlastName = request.getParameter("lastname");
-       // System.out.println(playerfirstName);
+        System.out.println(playerfirstName);
         request.setAttribute("firstname", playerfirstName);
         request.setAttribute("lastname", playerlastName);
         request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
+    }*/
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        request.getRequestDispatcher("/WEB-INF/views/welcome.jsp").forward(request, response);
+    }
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        String playerName = request.getParameter("name");
+        request.setAttribute("name", playerName);
+        request.getRequestDispatcher("/WEB-INF/views/info.jsp").forward(request, response);
     }
 }
